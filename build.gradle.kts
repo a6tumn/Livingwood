3912fffa0b7934d5f7233a5423198dc88feed423
autumn <shinigami7x@proton.me>
2026-04-09 20:54:40 -0400

    Updated supported version and added maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "maven-publish")
+    apply(plugin = "com.vanniktech.maven.publish")
 
     extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension> {
         jvmToolchain(25)
     }
 
-    plugins.withId("java") {
-        extensions.configure<JavaPluginExtension> {
-            withSourcesJar()
-            withJavadocJar()
-        }
+    mavenPublishing {
+        publishToMavenCentral()
+        signAllPublications()
     }
 }
